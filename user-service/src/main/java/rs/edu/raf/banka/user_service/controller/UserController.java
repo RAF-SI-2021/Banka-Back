443a2e2ec3a852323763ed55e917fb6dd569702c
mrkkvc <mkovac10220rn@raf.rs>
2022-04-12 02:58:19 +0200

    Test user-service

--- user-service/src/main/java/rs/edu/raf/banka/user_service/controller/UserController.java
+++ user-service/src/main/java/rs/edu/raf/banka/user_service/controller/UserController.java
@@ -46,7 +46,9 @@ public class UserController {
     public ResponseEntity<?>deleteUser(@PathVariable long id) {
         Optional<User> user = userService.getUserById(id);
         if(user.get() == null){ResponseEntity.badRequest().build();}
-        userService.deleteUser(user.get());
+        if(!userService.deleteUser(user.get())){
+            return ResponseEntity.badRequest().body("Can't delete admin");
+        }
         return ResponseEntity.ok().body(user.get().getUsername() + " disabled");
     }
 
@@ -54,9 +56,9 @@ public class UserController {
     @ApiOperation("Edit user with specific id,text fields are with existing data, the user can change them")
     @ApiResponses(value = {@ApiResponse(code = 200, message = "OK", response = User.class)})
     public ResponseEntity<?>editUser(@PathVariable long id, @RequestHeader("Authorization") String token, @RequestBody CreateUserForm createUserForm) {
-        var user = userService.getUserById(id).get();
-        if(!userService.hasEditPermissions(user, token))
-            return ResponseEntity.badRequest().build();
+        User user = userService.getUserById(id).get();
+        if (!userService.hasEditPermissions(user, token))
+        return ResponseEntity.badRequest().build();
         //if(user.isRequiresOtp())
         //    return ResponseEntity.badRequest().build();
 
