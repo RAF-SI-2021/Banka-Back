e52bcb1ad6c103d27d40468d83c807870e423890
Lazar Miladinovic <64776412+lmiladinovic99@users.noreply.github.com>
2022-06-21 00:13:58 +0200

    Revert "Add Company entities, controller, repos, and services"

--- racun-service/src/main/java/rs/edu/raf/banka/racun/repository/company/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package rs.edu.raf.banka.racun.repository.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rs.edu.raf.banka.racun.model.company.Company;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-
-}
